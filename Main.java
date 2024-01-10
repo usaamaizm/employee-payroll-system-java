3becce9810c51fa409e7b53a09055fe45e1861a4
Muhammad Usama <73215077+usaamaizm@users.noreply.github.com>
2024-01-10 22:18:36 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+abstract class Employee {
+
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId (){
+        return id;
+    }
+
+     public abstract double calculateSalary ();
+    @Override
+    public String toString (){
+        return "Employee[name="+name+", id=
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
